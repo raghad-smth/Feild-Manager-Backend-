45bf191f504e5162c0a839674d3ef2cf2b725571
raghad-smth <143431529+raghad-smth@users.noreply.github.com>
2025-08-21 16:01:46 +0300

    initial project structers with controller, service, repo, model folders

---
+++ feilds/src/main/java/com/example/feilds/FeildsApplication.java
@@ -0,0 +1,15 @@
+package com.example.feilds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class FeildsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeildsApplication.class, args);
+	}
+
+}
+
